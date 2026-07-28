a88ce6ce4f8eb4d13c14d124cde3c2aa43d746f0
dev_pae <panompria11@gmail.com>
2026-07-28 21:04:02 +0700

    pae : change config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "oxemxjxvyymejjxcubxo.supabase.co",
+        // dev
+        // hostname: "oxemxjxvyymejjxcubxo.supabase.co",
+        // prd
+        hostname: "monvsxbkhlvlhiacdmnc.supabase.co",
       },
     ],
   },
